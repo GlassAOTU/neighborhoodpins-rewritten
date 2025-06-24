55624047da17b17270da849581ba6ed23459e875
Abaseen Khan <arbaseen@gmail.com>
2025-06-24 11:22:54 -0400

    added town data to map, along with map controls

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { useEffect, useRef } from "react";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -25,9 +27,64 @@ export default function Map() {
                     [-74.056, 40.535],
                     [-71.85, 41.197],
                 ],
-            });
+            })
+
+            map.doubleClickZoom.disable()
+            // adds controls for the zoom and compass buttons
+            map.addControl(new mapboxgl.NavigationControl())
+            // adds control for locate me button
+            map.addControl(
+                new mapboxgl.GeolocateControl({
+                    mapboxgl: mapboxgl,
+                    positionOptions: {
+                        enableHighAccuracy: true,
+                    },
+                    trackUserLocation: true,
+                })
+            )
+
+            map.addControl(
+                new MapboxGeocoder({
+                    mapboxgl: mapboxgl,
+                    accessToken: process.env.NEXT_PUBLIC_MAPBOX_API_KEY!,
+                    bbox: [-73.798552, 40.55391, -71.900128, 41.260419],
+                }),
+                'top-left'
+            )
+
+            map.on('load', () => {
+                // loads the town data source layer
+                map.addSource('town-data', {
+                    type: 'geojson',
+                    data: './towns-map.json',
+                })
+
+                map.addLayer({
+                    id: 'towns-fill',
+                    type: 'fill',
+                    source: 'town-data',
+                    paint: {
+                        'fill-color': '#2ca060',
+                        'fill-opacity': 0.05,
+                    },
+                })
+
+                map.addLayer({
+                    id: 'towns-outline',
+                    type: 'line',
+                    source: 'town-data',
+                    paint: {
+                        'line-color': '#165030',
+                        'line-width': 2,
+                    },
+                })
+            })
+
         }
+
     }, []);
+
+
 
     return (
         <div
