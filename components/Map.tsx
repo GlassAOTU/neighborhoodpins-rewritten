fc82ff42f51aecfc903d83c3ffff7710552b97ce
Abaseen Khan <arbaseen@gmail.com>
2025-06-27 03:21:34 -0400

    mouse adjustments and commenting

--- components/Map.tsx
+++ components/Map.tsx
@@ -12,9 +12,12 @@ export default function Map() {
     const mapRef = useRef(null)
 
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+
+    // actual map creation
     useEffect(() => {
 
         if (mapContainer.current) {
+            // creates the map, but doesn't load it
             const map = new mapboxgl.Map({
                 container: mapContainer.current as HTMLElement,
                 style: 'mapbox://styles/mapbox/dark-v11',
@@ -29,13 +32,18 @@ export default function Map() {
                 ],
             })
 
+            // mouse hover now shows crosshair
+            map.getCanvas().style.cursor = 'crosshair';
+
+            // disables double click zoom
             map.doubleClickZoom.disable()
-            // adds controls for the zoom and compass buttons
+
+            // adds zoom and compass controls
             map.addControl(new mapboxgl.NavigationControl())
-            // adds control for locate me button
+
+            // adds a current location button
             map.addControl(
                 new mapboxgl.GeolocateControl({
-                    mapboxgl: mapboxgl,
                     positionOptions: {
                         enableHighAccuracy: true,
                     },
@@ -43,8 +51,10 @@ export default function Map() {
                 })
             )
 
+            // adds search bar to the top left corner of the map
             map.addControl(
                 new MapboxGeocoder({
+                    // @ts-ignore -- code works fine, TS throws errors, ignore for now
                     mapboxgl: mapboxgl,
                     accessToken: process.env.NEXT_PUBLIC_MAPBOX_API_KEY!,
                     bbox: [-73.798552, 40.55391, -71.900128, 41.260419],
@@ -52,6 +62,7 @@ export default function Map() {
                 'top-left'
             )
 
+            // map is loaded onto the page
             map.on('load', () => {
                 // loads the town data source layer
                 map.addSource('town-data', {
@@ -59,6 +70,7 @@ export default function Map() {
                     data: './towns-map.json',
                 })
 
+                // visualizes the town area
                 map.addLayer({
                     id: 'towns-fill',
                     type: 'fill',
@@ -69,6 +81,7 @@ export default function Map() {
                     },
                 })
 
+                // visualized the town outlines
                 map.addLayer({
                     id: 'towns-outline',
                     type: 'line',
