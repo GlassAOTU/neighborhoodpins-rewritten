4a2ec62feb8203527d7461b7f794e53489ea8b29
Abaseen Khan <arbaseen@gmail.com>
2025-07-08 16:33:35 -0400

    added account redirect if logged in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ export default async function Navbar() {
                     </Link>
 
                     {session ?
-                        <Link href='/auth/logout' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
+                        <Link href='/account' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="8" r="5" /><path d="M20 21a8 8 0 0 0-16 0" /></svg>
                             <p className='text-xs sm:text-sm'>Account</p>
                         </Link> :
