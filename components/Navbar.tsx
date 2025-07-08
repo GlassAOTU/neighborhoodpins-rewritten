bfeff5ab0662092a0ae9acc2521220bc83741330
Abaseen Khan <arbaseen@gmail.com>
2025-07-07 21:17:29 -0400

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,12 +19,12 @@ export default async function Navbar() {
                         <p className='text-xs sm:text-sm '>Map</p>
                     </Link>
 
-                    <Link href='/' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
+                    <Link href='/reports' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
                         <svg xmlns="http://www.w3.org/2000/svg" className="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M13 12h8" /><path d="M13 18h8" /><path d="M13 6h8" /><path d="M3 12h1" /><path d="M3 18h1" /><path d="M3 6h1" /><path d="M8 12h1" /><path d="M8 18h1" /><path d="M8 6h1" /></svg>
                         <p className='text-xs sm:text-sm'>Reports</p>
                     </Link>
 
-                    <Link href='/' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
+                    <Link href='/resources' className='text-glow flex flex-col items-center drop-shadow-black hover:drop-shadow-black hover:drop-shadow-md drop-shadow-sm/50 hover:scale-110 transition-all'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 7v14" /><path d="M16 12h2" /><path d="M16 8h2" /><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" /><path d="M6 12h2" /><path d="M6 8h2" /></svg>
                         <p className='text-xs sm:text-sm'>Resources</p>
                     </Link>
