a3e2aa2ad3e331ffb6663ae23ef6515535e90846
Abaseen Khan <arbaseen@gmail.com>
2025-07-09 14:22:47 -0400

    finished the reports page for now

--- app/(pages)/reports/page.tsx
+++ app/(pages)/reports/page.tsx
@@ -2,8 +2,13 @@ import ReportsTable from "@/components/ReportsTable";
 
 export default function Reports() {
     return (
-        <main className="bg-gray-50 h-screen">
-            <ReportsTable />
+        <main className="min-h-screen bg-stone-100 px-4 py-10">
+            <section className="max-w-4xl mx-auto flex flex-col gap-8">
+                <div>
+                <h1 className="text-3xl font-bold text-center mb-8 text-stone-800">All Reported Issues</h1>
+                <ReportsTable />
+                </div>
+            </section>
         </main>
     )
 }
